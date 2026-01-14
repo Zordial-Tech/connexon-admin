035cdde60246d9b54bf85136463ec23fe01babcc
Mayank Aitan <hunny.bohara@zordial.com>
2026-01-14 23:41:21 +0530

    token fix

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem("token"); // or your auth logic
+  const isAuthenticated = localStorage.getItem("authToken"); // or your auth logic
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
